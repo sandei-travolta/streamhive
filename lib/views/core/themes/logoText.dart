7c1ce2469fd2fe64e4df2b7c9c3381d90e6c1057
Travolta Sandei <67733769+sandei-travolta@users.noreply.github.com>
2025-10-10 18:25:32 +0300

    app bar implementation

---
+++ lib/views/core/themes/logoText.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+Text logoText(BuildContext context) => Text(
+    "Stream Hive",
+  style: TextStyle(
+    letterSpacing: 0.2,
+    fontSize: 22,
+    fontWeight: FontWeight.w700
+  ),
+);
--- lib/views/home-screen/home_screen.dart
+++ lib/views/home-screen/home_screen.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:streamhive/views/core/themes/logoText.dart';
 class Homescreen extends StatelessWidget {
   const Homescreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: logoText(context),
+        actions: [
+          Container(
+
+          )
+        ],
+      ),
+      drawer: Drawer(),
+    );
   }
 }
